aa5f5f8acd5bc6b3e4fbfde90e81aca00cfaaba5
EKgurumin <kenari1984@yahoo.co.jp>
2023-03-08 19:27:57 +0900

    Initial Commit

---
+++ quiz_challenge/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  quiz_challenge
+//
+//  Created by 株式会社ジェーディーエス on 2023/03/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ quiz_challenge/quiz_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  quiz_challengeApp.swift
+//  quiz_challenge
+//
+//  Created by 株式会社ジェーディーエス on 2023/03/08.
+//
+
+import SwiftUI
+
+@main
+struct quiz_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
