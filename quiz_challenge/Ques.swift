a7f40df5b335c908348e967aef1980b3b2f9728c
EKgurumin <kenari1984@yahoo.co.jp>
2023-03-24 20:20:14 +0900

    問題文を追加

--- quiz_challenge/Ques.swift
+++ quiz_challenge/Ques.swift
@@ -9,15 +9,12 @@ import Foundation
 
 class Ques{
     init(){}
-    var question:[String] = ["清水寺がある都道府県は？\n A.京都府\n B.大阪府\n C.滋賀県\n D.奈良県",
-                             "比叡山延暦寺がある都道府県は？\n  A.京都府\n B.大阪府\n C.滋賀県\n D.奈良県"]
-    var answer:[String] = ["A","C"]
+    var question:[String] = [
+        "清水寺がある都道府県は？\n A.京都府\n B.大阪府\n C.滋賀県\n D.奈良県",
+        "比叡山延暦寺がある都道府県は？\n  A.京都府\n B.大阪府\n C.滋賀県\n D.奈良県",
+        "東京都小笠原諸島に芝浦から船で出発した場合、およそ何時間かかる？\n A.6時間\n B.12時間\n C.24時間\n D.36時間",
+        "元素記号An,Cn,C,Agの中で、「銅」を表すのはどれ？\n A.An\n B.Cn\n C.C\n D.Ag"]
+    var answer:[String] = ["A","C","C","B"]
     
     
 }
-
-var ques = Ques()
-var questionsForUsers = ques.question
-var answerForUsers = ques.answer
-
-//print(questionsForUsers.randomElement()!)
