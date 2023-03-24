62adf6cbe466d354d466e71d3f311f2a2873a533
EKgurumin <kenari1984@yahoo.co.jp>
2023-03-24 20:34:17 +0900

    答えが表示できるようになって嬉しい。

--- quiz_challenge/ContentView.swift
+++ quiz_challenge/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var questionCounter = 0
-    
+    @State var answerDisplay = ""
     
     
     var body: some View {
@@ -25,25 +25,48 @@ struct ContentView: View {
         
         VStack {
             Text(questionsForUsers[questionCounter])
+            Text(answerDisplay)
             Button("A"){
+                if ques.answer[questionCounter] == "A"{
+                    answerDisplay = "正解です！"
+                }else{
+                    answerDisplay = "残念！正解は\(ques.answer[questionCounter])でした"
+                }
                 
             }
             .padding()
             Button("B"){
+                if ques.answer[questionCounter] == "B"{
+                    answerDisplay = "正解です！"
+                }else{
+                    answerDisplay = "残念！正解は\(ques.answer[questionCounter])でした"
+                }
+                
                 
             }
             .padding()
             Button("C"){
+                if ques.answer[questionCounter] == "C"{
+                    answerDisplay = "正解です！"
+                }else{
+                    answerDisplay = "残念！正解は\(ques.answer[questionCounter])でした"
+                }
                 
             }
             .padding()
             Button("D"){
+                if ques.answer[questionCounter] == "D"{
+                    answerDisplay = "正解です！"
+                }else{
+                    answerDisplay = "残念！正解は\(ques.answer[questionCounter])でした"
+                }
                 
             }
             .padding()
             Button("次の問題"){
                 if ques.question.count != (1 + questionCounter){
                     questionCounter = questionCounter + 1
+                    answerDisplay = ""
                 }else{
                     print("もう要素なし")
                 }
